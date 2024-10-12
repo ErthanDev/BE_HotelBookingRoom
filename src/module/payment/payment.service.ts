ede4f513f04814287abc4bbbb757033a1ba434ed
ADMIN <tranminhtri10213@gmail.com>
2024-10-12 09:22:55 +0700

    get revenue and payment by cash

--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { Booking } from 'src/module/booking/entities/booking.entity';
 import { Discount } from 'src/module/discount/entities/discount.entity';
 import { DiscountStatus } from 'src/enum/discountStatus.enum';
@@ -10,6 +10,7 @@ import { BookingStatus } from 'src/enum/bookingStatus.enum';
 import { MailService } from 'src/module/mail/mail.service';
 import { CreateMailDto } from 'src/module/mail/dto/create-mail.dto';
 import { PaymentStatus } from 'src/enum/paymentStatus.enum';
+import * as moment from 'moment';
 
 
 @Injectable()
@@ -25,7 +26,7 @@ export class PaymentService {
   ) { }
   async createPayment(createPaymentDto: CreatePaymentDto) {
     const { bookingId, amount, paymentMethod, discountCode } = createPaymentDto;
-  
+
     // Tìm discount nếu có
     if (discountCode) {
       let discount = await this.discountRepository.findOne({ where: { discountCode } });
@@ -43,7 +44,7 @@ export class PaymentService {
       discount.discountStatus = DiscountStatus.Unavailable;
       await this.discountRepository.save(discount);
     }
-  
+
     // Tìm booking
     const booking = await this.bookingRepository.findOne({
       where: { bookingId },
@@ -54,7 +55,7 @@ export class PaymentService {
     }
     booking.bookingStatus = BookingStatus.Paid;
     await this.bookingRepository.save(booking);
-  
+
     // Tạo payment
     const payment = this.paymentRepository.create({
       amount,
@@ -78,7 +79,7 @@ export class PaymentService {
     }, 0);  // Đưa vào queue thực hiện ngay khi có thể
 
     // Extract user information without refreshToken
-    const { refreshToken,password, ...userWithoutToken } = booking.user;
+    const { refreshToken, password, ...userWithoutToken } = booking.user;
 
     return {
       amount: savedPayment.amount,
@@ -91,7 +92,7 @@ export class PaymentService {
         bookingStatus: booking.bookingStatus,
         bookingDate: booking.bookingDate,
         numberOfGuest: booking.numberOfGuest,
-        user: userWithoutToken, 
+        user: userWithoutToken,
       },
       paymentId: savedPayment.paymentId,
       paymentDate: savedPayment.paymentDate,
@@ -100,25 +101,25 @@ export class PaymentService {
   }
 
   async refundPayment(paymentId: string) {
-    const payment = await this.paymentRepository.findOne({ where: { paymentId },relations: ['booking'] });
+    const payment = await this.paymentRepository.findOne({ where: { paymentId }, relations: ['booking'] });
     if (!payment) {
       throw new NotFoundException('Payment not found');
     }
-  
+
     const booking = await this.bookingRepository.findOne({ where: { bookingId: payment.booking.bookingId } });
     if (!booking) {
       throw new NotFoundException('Booking not found');
     }
-  
+
     const now = new Date();  // Lấy ngày hiện tại
     const startTime = new Date(booking.startTime);  // Lấy thời gian bắt đầu của booking
-  
+
     // Tính khoảng cách ngày giữa ngày hiện tại và startTime
     const diffTime = startTime.getTime() - now.getTime();
     const daysUntilStart = Math.ceil(diffTime / (1000 * 60 * 60 * 24));  // Chuyển đổi mili giây thành ngày
-  
+
     let refundPercentage = 0;  // Tỷ lệ hoàn tiền mặc định là 0%
-  
+
     if (daysUntilStart >= 7) {
       refundPercentage = 100;  // Hoàn 100% nếu startTime cách ngày hiện tại 7 ngày trở lên
     } else if (daysUntilStart >= 3) {
@@ -126,15 +127,15 @@ export class PaymentService {
     } else {
       refundPercentage = 0;  // Không hoàn nếu startTime cách ngày hiện tại dưới 3 ngày
     }
-  
+
     // Cập nhật trạng thái booking và payment
     booking.bookingStatus = BookingStatus.Cancelled;
     await this.bookingRepository.save(booking);
-    
+
     payment.paymentStatus = PaymentStatus.Refund;
-    payment.amount = payment.amount-(payment.amount * refundPercentage / 100 ); // Tính lại số tiền hoàn
+    payment.amount = payment.amount - (payment.amount * refundPercentage / 100); // Tính lại số tiền hoàn
     await this.paymentRepository.save(payment);
-  
+
     return {
       paymentId: payment.paymentId,
       paymentStatus: payment.paymentStatus,
@@ -151,9 +152,48 @@ export class PaymentService {
       },
     };
   }
-  
+
 
   async findAll() {
     return await this.paymentRepository.find();
   }
+
+  async getRevenueByDay(startDate: string, endDate: string) {
+    const payments = await this.paymentRepository
+      .createQueryBuilder('payment')
+      .select("DATE(payment.paymentDate)", "date")  // Nhóm theo ngày
+      .addSelect("SUM(payment.amount)", "totalRevenue")  // Tính tổng doanh thu cho mỗi ngày
+      .where("payment.paymentDate BETWEEN :startDate AND :endDate", {
+        startDate: new Date(startDate),
+        endDate: new Date(endDate),
+      })
+      .groupBy("DATE(payment.paymentDate)")  // Nhóm kết quả theo ngày
+      .getRawMany();  // Trả về kết quả thô
+
+    return payments;
+  }
+
+  async getRevenueByMonth(year:number) {
+    const payments = await this.paymentRepository
+      .createQueryBuilder('payment')
+      .select("DATE_FORMAT(payment.paymentDate, '%Y-%m')", "month")  // Nhóm theo tháng
+      .addSelect("SUM(payment.amount)", "totalRevenue")  // Tính tổng doanh thu cho mỗi tháng
+      .where("YEAR(payment.paymentDate) = :year", { year })
+      .groupBy("DATE_FORMAT(payment.paymentDate, '%Y-%m')")  // Nhóm kết quả theo tháng
+      .getRawMany();  // Trả về kết quả thô
+
+    return payments;
+  }
+
+  async getRevenueByYear() {
+    const payments = await this.paymentRepository
+      .createQueryBuilder('payment')
+      .select("YEAR(payment.paymentDate)", "year")  // Nhóm theo năm
+      .addSelect("SUM(payment.amount)", "totalRevenue")  // Tính tổng doanh thu cho mỗi năm
+      .groupBy("YEAR(payment.paymentDate)")  // Nhóm kết quả theo năm
+      .getRawMany();  // Trả về kết quả thô
+
+    return payments;
+  }
+
 }
